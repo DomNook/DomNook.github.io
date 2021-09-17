79a71fc30c0d512c09672bb7502c81b6007c11a8
DomNook <ddd365rocks@gmail.com>
2021-09-17 19:14:14 +0200

    scripted in canvas

---
+++ canvas.js
@@ -0,0 +1,156 @@
+function draw() {
+    var canvas = document.getElementById("canvastest");
+    if (canvas.getContext) {
+        var ctx = canvas.getContext("2d");
+
+        //White squares here
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(20, 20, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(220, 20, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(420, 20, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(620, 20, 100, 100)
+
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(120, 120, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(320, 120, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(520, 120, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(720, 120, 100, 100)
+
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(20, 220, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(220, 220, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(420, 220, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(620, 220, 100, 100)
+
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(120, 320, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(320, 320, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(520, 320, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(720, 320, 100, 100)
+
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(20, 420, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(220, 420, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(420, 420, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(620, 420, 100, 100)
+
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(120, 520, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(320, 520, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(520, 520, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(720, 520, 100, 100)
+        
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(20, 620, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(220, 620, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(420, 620, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(620, 620, 100, 100)
+
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(120, 720, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(320, 720, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(520, 720, 100, 100)
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(720, 720, 100, 100)
+
+        //Brown squares here
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(120, 20, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(320, 20, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(520, 20, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(720, 20, 100, 100)
+
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(20, 120, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(220, 120, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(420, 120, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(620, 120, 100, 100)
+
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(120, 220, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(320, 220, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(520, 220, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(720, 220, 100, 100)
+
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(20, 320, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(220, 320, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(420, 320, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(620, 320, 100, 100)
+
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(120, 420, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(320, 420, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(520, 420, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(720, 420, 100, 100)
+
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(20, 520, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(220, 520, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(420, 520, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(620, 520, 100, 100)
+
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(120, 620, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(320, 620, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(520, 620, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(720, 620, 100, 100)
+
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(20, 720, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(220, 720, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(420, 720, 100, 100)
+        ctx.fillStyle = "rgb(150, 75, 0)";
+        ctx.fillRect(620, 720, 100, 100)
+
+
+        // Queen
+
+    }
+}
